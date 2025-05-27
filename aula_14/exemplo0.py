9bb237d3b8c0f6db31286d64377e4bc7efb4166a
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-27 19:16:00 -0300

    info sobre mysql

--- aula_14/exemplo0.py
+++ aula_14/exemplo0.py
@@ -1,3 +1,10 @@
 # DQL>MYSQL>MARIADB
 #SÃO FERRAMENTAS PARA ADMINISTRAÇÃO DE BANCOS DE DADOS
-#
+#teste no cmd pra saber se está instalado
+'''C:\Users\51134552025.1>mysql -u root -p'''
+#sem iniciar o XAMPP não vai funcionar
+
+'''no mysql
+    >hostname comeca com 127.0.0.1 por padrão e sempre é local
+    >Port padrao: 3306 (recomendável sempre trocar) 
+    >pra teste não é necessário incluir um senha'''
