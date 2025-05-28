862271feaaca0d0a9c6085462de04d2ed2b9ffba
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-27 21:29:00 -0300

    final da aula

--- aula_14/exemplo1.py
+++ aula_14/exemplo1.py
@@ -1,6 +1,28 @@
-#pi install sqlachemy pymysql
+#pip install sqlachemy pymysql
 
 from sqlalchemy import create_engine
 
 #variaveis de conexão
+host = 'localhost'
+user = 'root'
+pasword = ''
+database = 'db_analise'
 
+#funçãopara realizar a conexão com o banco
+
+
+def conecta_banco():
+    try:
+        #  url de conexão com o banco
+        engine = create_engine(f'mysql+pymysql://{user}:{pasword}@{host}/{database}')
+
+        with engine.connect() as conexao:
+            query = "SELECT *FROM vendas2"
+
+            resultado = conexao.execute(text(query))
+            if resultado.rowcount > 0:
+                for item in resultado:
+                    print (item) #  imprime todas as coluna
+                    print (item[0], item[1],item[2])
+    except Exception as e:
+        print (f'Algo deu errado: {e}')
